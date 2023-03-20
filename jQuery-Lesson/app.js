3c90c7d28adb8d02a669cb28a287658caa12222e
{natsumikobayashi} <ec2-user@ip-172-31-28-237.ap-northeast-1.compute.internal>
2023-03-20 11:48:42 +0000

    [Finish]java-3.java-5

---
+++ jQuery-Lesson/app.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'heigth': '100px',
+      'width': '200px'
+  }).slideUp();
+  });
+});
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
