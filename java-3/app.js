14ae4500a26cc6f50a29f3ae720b7d5794e07c51
natsumikobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2023-09-05 15:18:08 +0000

    [Add]console

--- java-3/app.js
+++ java-3/app.js
@@ -6,7 +6,9 @@ console.log(nickname)
 // １～１０偶数のみ出力
 for(let num = 1; num <=10; num++){
   if(num % 2 === 0){
+    const li = `<li>${num}</li>`;
     console.log(num);
+    document.getElementById('list').insertAdjacentHTML('beforeend', li);
   }
 }
 
@@ -24,4 +26,7 @@ console.log(count);
 console.log(total);
 
 //1～10までの整数をカンマ区切りで横並びに表示させる。10の後ろにはカンマ付けない
-
+// let num = ['1','2','3','4','5','6','7','8','9','10'];
+// for(let date of num){
+//   console.log(date);
+// }
