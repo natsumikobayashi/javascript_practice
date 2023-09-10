aa37b6fe441380d50e5e7f8131ab21808253cb1d
natsumikobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2023-09-10 14:56:08 +0000

    [Add]console

--- java-3/app.js
+++ java-3/app.js
@@ -31,3 +31,37 @@ for(var num = 1; num <= 10; num++) {
         console.log(num);
       }
     }
+    
+let number = [20];
+
+console.log(number);
+// for(let max = 20; max<= number.length;){
+// 	if(){
+		
+// 	}
+// }
+
+
+// int number[] = new int[20]; //20の要素を作成
+// 			String str ="";
+// 			number[0] = 0;
+// 			for(int i = 1; i < number.length; i++) {
+// 				number[i] = number[i - 1] + 5; //0から5ずつnumber[i]に加算
+// 			}
+
+// 			System.out.print("奇数 : ");
+// 			for (int i = 0; i < 11; i++) {
+// 				if (number[i] % 2 != 0) {
+// 					str += String.valueOf(number[i]) + ",";
+// 				}
+// 			}
+
+// 			System.out.println(str.substring(0, str.length() - 1));
+// 			System.out.print("偶数 : ");
+// 			str = "";
+// 			for (int i = 11; i < 20; i++) {
+// 				if (number[i] % 2 == 0) {
+// 					str += String.valueOf(number[i]) + ",";
+// 				}
+// 			}
+// 			System.out.println(str.substring(0, str.length() - 1));
